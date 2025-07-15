780e8dc8349fe558d62d2f719cbd53b31354853d
Haziq739 <naeemhaziq345@gmail.com>
2025-07-15 15:10:48 +0500

    Imported Regex from constants file into Validators file

--- Backend/src/utils/validators.ts
+++ Backend/src/utils/validators.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'; // Importing third party package
+import { EMAIL_REGEX } from '../utils/constants'; // Importing regex from constants
 dotenv.config();
 
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN as string;
 
-// Regex for proper email validation
-export const validateEmail = (email: string) => {
-  const re = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\\.,;:\s@\"]+\.)+[^<>()[\]\\.,;:\s@\"]{2,})$/i;
-  return re.test(email);
+
+// Import regex for validation
+export const validateEmail = (email: string): boolean => {
+  return EMAIL_REGEX.test(email);
 };
