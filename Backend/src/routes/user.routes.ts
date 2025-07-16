8b67c82b24edf1a2bf3daf0fc4959daee706c1a4
Haziq739 <naeemhaziq345@gmail.com>
2025-07-17 02:02:41 +0500

    Added Dashboard file

--- Backend/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'; // Importing third party package
-import { registerUser, loginUser } from '../controllers/user.controller';
-
-const router = Router();
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-export default router;
--- Backend/src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-// src/config/constants.ts
-
-// Email regex constant
-
-export const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\\.,;:\s@\"]+\.)+[^<>()[\]\\.,;:\s@\"]{2,})$/i;
