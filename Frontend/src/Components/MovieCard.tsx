1c5e7d1fda41f5a41b9615c52fb8dc571f9b8843
Haziq739 <naeemhaziq345@gmail.com>
2025-07-23 17:13:56 +0500

    Added comment to MovieCard.tsx

--- Frontend/src/Components/MovieCard.tsx
+++ Frontend/src/Components/MovieCard.tsx
@@ -1,4 +1,5 @@
 // src/Components/MovieCard.tsx
+// Importing Third Party Packages
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { OMDbSearchResult, OMDbMovieDetails } from '../types/omdb';
